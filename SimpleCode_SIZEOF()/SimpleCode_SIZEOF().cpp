2a2df51770f8e5539d670211a5b02986f0e1a8e0
tokaretin <tokaretin@gmail.com>
2023-05-24 14:34:34 +0200

    Добавьте файлы проекта.

---
+++ SimpleCode_SIZEOF()/SimpleCode_SIZEOF().cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int arr[]{ 5,6,8,8,4,5,9,6,8 };
+
+    int a = sizeof(arr); // найдет сколько байт хранинтся в массиве
+    int b = sizeof(arr[0]); // покажет сколько хранится в ожном индексе памяти в байтах
+
+    std::cout << "Всего элементов в массиве " << a / b << '\n';
+
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        std::cout << "Индекс " << i << " содержит " << arr[i]<< '\n';
+        std::cout << "Всего элементов в массиве " << sizeof(arr) / sizeof(arr[0]);
+    }
+}
